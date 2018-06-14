17f43ab9fa08228ba47c52af1c2d2bc14a873a8d
Henry Allsuch <henry@agilepixel.io>
2018-06-14 21:21:22 +0100

    Initial Commit

---
+++ BrassBitCoin/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  BrassBitCoin
+//
+//  Created by Saoirse on 14/06/2018.
+//  Copyright © 2018 YorkDojo. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
